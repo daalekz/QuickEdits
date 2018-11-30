03c4425b6b28bcc3d99e6539548b08690688c088
daalekz <dontdissthesonic123@gmail.com>
2018-11-30 11:54:40 +1100

    t da

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #QuickEdits
 *.log
 *.lock
+listpeople/.vs/listpeople/v15/Server/sqlite3/db.lock
---
+++ listpeople/listpeople/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace listpeople
+{
+    class Entity
+    {
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                Name = value;
+            }
+
+        }
+        public Entity(string name)
+        {
+            _name = name;
+
+        }
+    }
+}
